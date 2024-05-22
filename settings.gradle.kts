e4a88b347e4b241e4265280786ff800ac6ce5a57
stephengold <sgold@sonic.net>
2024-05-22 09:12:07 -0700

    buildscripts:  comments and whitespace

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-// global settings for the BasicGame-on-Kotlin project
+// global build settings for the BasicGame-on-Kotlin project
 
 rootProject.name = "BasicGame-on-Kotlin"
 
@@ -6,14 +6,16 @@ dependencyResolutionManagement {
     repositories {
         //mavenLocal() // to find libraries installed locally
         //flatDir { dirs("lib") } // to find libraries in the project's "lib" directory
-        mavenCentral()
+        mavenCentral() // to find libraries released to the Maven Central repository
         maven { url = uri("https://jitpack.io") } // to find the Blocks library
         //maven { url = uri("https://s01.oss.sonatype.org/content/groups/staging") } // to find libraries staged but not yet released
         //maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots") } // to find public snapshots of libraries
         //maven { url = uri("https://nifty-gui.sourceforge.net/nifty-maven-repo") }
-        //google()
+        //google() // to find libraries released to Google's Maven Repository
 
         // Read more about repositories here:
         //   https://docs.gradle.org/current/userguide/dependency_management.html#sec:repositories
     }
 }
+
+// no subprojects
